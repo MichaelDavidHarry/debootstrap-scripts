7715fd110d960cd8b6d6c97bed9b600d95b19340
Michael Harry <michael@compilefailed.com>
2021-07-24 14:56:45 +0000

    Update build2.sh script to be able to build both EFI and BIOS installs

--- build2.sh
+++ build2.sh
@@ -4,7 +4,6 @@
 FILE_BLOCK_DEVICE_SIZE=3G
 HOSTNAME=debian-vm
 BLOCK_DEVICE_CRYPT_PARTITION=/dev/loop0p1
-BLOCK_EFI_PARTITION=/dev/loop0p1
 BLOCK_DEVICE=/dev/loop0
 USER_NAME=user
 USER_PASSWORD=password
@@ -15,13 +14,16 @@ CRYPT_DM_NAME="cryptlvm"
 LVM_VG_NAME="vg"
 ENCRYPTION_PASSWORD="crypt"
 SWAP_SIZE=128M
+
+# EFI-specific options
 USE_EFI=true
+BLOCK_EFI_PARTITION=/dev/loop0p1
 
 # Stop on errors
 set -e
 
 # When EFI is in use, the first partition is used as the EFI partition, and the second partition is the encrypted root
-if [ $USE_EFI ];
+if [ "$USE_EFI" = true ];
 then
 	BLOCK_DEVICE_CRYPT_PARTITION=/dev/loop0p2
 fi
@@ -29,15 +31,16 @@ fi
 echo "Prepping file block device"
 truncate -s $FILE_BLOCK_DEVICE_SIZE debian.dd
 sudo losetup --partscan --show --find debian.dd
-if [ $USE_EFI ];
+if [ "$USE_EFI" = true ];
 then
+	echo "Create EFI partition and root partition"
 	echo $',100M,ef\n,,83' | sudo sfdisk /dev/loop0
 	sudo mkfs.fat -F32 $BLOCK_EFI_PARTITION
+	EFI_BLOCK_DEVICE_UUID=`blkid -o value -s UUID "$BLOCK_EFI_PARTITION"`
 else
+	echo "Create root partiton"
 	echo 'type=83' | sudo sfdisk "$BLOCK_DEVICE"
 fi
-
-EFI_BLOCK_DEVICE_UUID=`blkid -o value -s UUID "$BLOCK_EFI_PARTITION"`
 
 echo "Create and open encrypted device"
 echo -n "$ENCRYPTION_PASSWORD" | cryptsetup --type luks1 -y -v luksFormat "$BLOCK_DEVICE_CRYPT_PARTITION" -d -
@@ -61,7 +64,12 @@ echo "Bootstrapping the OS"
 set +e
 mkdir debootstrap-cache
 set -e
-sudo debootstrap --arch amd64 --cache-dir `pwd`/debootstrap-cache --include grub2,grub-efi-amd64-bin,linux-image-amd64,btrfs-progs,sudo,lvm2,cryptsetup-bin,cryptsetup-initramfs,cryptsetup-run buster mnt/ https://deb.debian.org/debian/ 
+PACKAGES="grub2,linux-image-amd64,btrfs-progs,sudo,lvm2,cryptsetup-bin,cryptsetup-initramfs,cryptsetup-run"
+if [ "$USE_EFI" = true ];
+then
+	PACKAGES="$PACKAGES,grub-efi-amd64-bin"
+fi
+sudo debootstrap --arch amd64 --cache-dir `pwd`/debootstrap-cache --include "$PACKAGES" buster mnt/ https://deb.debian.org/debian/ 
 
 echo "Copying template files"
 sudo cp -r template-files/* mnt
@@ -74,7 +82,7 @@ echo "Setting up fstab"
 echo "/dev/mapper/$LVM_VG_NAME-root	/	btrfs	defaults	0	0"  >> mnt/etc/fstab
 echo "/dev/mapper/$LVM_VG_NAME-swap   none   swap   defaults   0   0" >> mnt/etc/fstab
 
-if [ $USE_EFI ];
+if [ "$USE_EFI" = true ];
 then
 	echo "UUID=$EFI_BLOCK_DEVICE_UUID	/boot/efi	vfat	defaults	0	0" >> mnt/etc/fstab
 fi
@@ -88,10 +96,16 @@ sudo mount -t sysfs /sys mnt/sys/
 sudo mount -o bind /dev mnt/dev/
 sudo mount -o bind /run mnt/run/
 
-if [ $USE_EFI ];
+if [ "$USE_EFI" = true ];
 then
 	mkdir -p mnt/boot/efi
 	sudo mount /dev/loop0p1 mnt/boot/efi
+fi
+
+GRUB_EFI_OPTIONS=""
+if [ "$USE_EFI" = true ];
+then
+	GRUB_EFI_OPTIONS="--target=x86_64-efi --efi-directory=/boot/efi --removable"
 fi
 
 sudo chroot mnt /bin/bash -c "tasksel install standard && \
@@ -99,7 +113,7 @@ echo \"$LOCALE\" >> /etc/locale.gen && \
 echo LANG=\"$LANG\" >> /etc/default/locale && \
 locale-gen && \
 echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub && \
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --root-directory=/ \"$BLOCK_DEVICE\" --removable && \
+grub-install $GRUB_EFI_OPTIONS --root-directory=/ \"$BLOCK_DEVICE\" && \
 update-grub && \
 echo \"root:$ROOT_PASSWORD\" | chpasswd && \
 useradd -m -s /bin/bash $USER_NAME && \
@@ -121,7 +135,7 @@ sudo umount mnt/proc/
 sudo umount mnt/sys/
 sudo umount mnt/dev/
 sudo umount mnt/run/
-if [ $USE_EFI ];
+if [ "$USE_EFI" = true ];
 then
 	sudo umount mnt/boot/efi
 fi
@@ -133,3 +147,4 @@ sudo cryptsetup luksClose "$CRYPT_DM_NAME"
 sudo losetup -d "$BLOCK_DEVICE"
 #sudo rm debian.dd
 rm -rf mnt
+sudo chmod 777 debian.dd
