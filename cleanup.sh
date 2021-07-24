75e771a9236e8a6ef37242c62aabe0417f75c400
Michael Harry <michael@compilefailed.com>
2021-07-24 14:59:09 +0000

    Replace build.sh with build2.sh. Cleanup other unused scripts.

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo umount mnt
-sudo vgchange -an /dev/vg
-sudo cryptsetup luksClose cryptlvm
-sudo losetup -d /dev/loop0
-sudo rm debian.dd
-rm -rf mnt
--- unchroot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo umount mnt/proc/
-sudo umount mnt/sys/
-sudo umount mnt/dev/
-sudo umount mnt/run/
